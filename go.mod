74c79e378604cf086de0f1b1f10887f7927ad2d6
Eric <ericfrancis1@proton.me>
2024-12-10 18:06:59 -0600

    add tweet fmt

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/EricFrancis12/twitter-autoposter
 go 1.22.6
 
 require (
+	github.com/EricFrancis12/stripol v0.0.0-20241202174442-52e8b4a438fd
 	github.com/google/uuid v1.6.0
+	github.com/mattn/go-sqlite3 v1.14.24
 	github.com/michimani/gotwi v0.16.1
 )
-
-require github.com/mattn/go-sqlite3 v1.14.24 // indirect
--- source.go
+++ source.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+
+	"github.com/EricFrancis12/stripol"
 )
 
 type SourceName string
@@ -35,10 +40,30 @@ func (r RssFeedSource) GetSourceName() SourceName {
 
 func (r RssFeedSource) FetchPosts() ([]Post, error) {
 	// TODO: ...
+
+	resp, err := http.Get(r.Url)
+	if err != nil {
+		return []Post{}, err
+	}
+	defer resp.Body.Close()
+
+	// Read the entire response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return []Post{}, err
+	}
+
+	fmt.Println(string(body))
+
 	return []Post{}, nil
 }
 
 func (r RssFeedSource) FmtPost(post Post) string {
-	// TODO: ...
-	return ""
+	s := stripol.New("{{", "}}")
+
+	s.RegisterVar("url", post.Url)
+	s.RegisterVar("escapedUrl", url.PathEscape(post.Url))
+	s.RegisterVar("title", post.Title)
+
+	return s.Eval(r.TweetFmt)
 }
